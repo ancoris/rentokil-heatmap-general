86d7f733cb8c8abcb70ef16bef51de3c457083df
John Gale <JohnGale87@users.noreply.github.com>
2024-04-26 12:58:41 +0100

    use a formatter for displaying the area

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import "./App.css";
 import { Map } from "./components/Map";
 
 function App() {
     const [featureArea, setFeatureArea] = useState<number>(0);
+    const [displayArea, setDisplayArea] = useState<string>("");
     const MemoizedMap = useMemo(() => {
         return <Map {...{ setFeatureArea }} />;
     }, [setFeatureArea]);
 
+    useEffect(() => {
+        console.log("Feature Area: ", featureArea);
+        setDisplayArea(
+            new Intl.NumberFormat("en-GB", {
+                // style: "unit",
+                // unit: "area-square-foot",
+                style: "decimal",
+                unitDisplay: "short",
+            }).format(featureArea),
+        );
+    }, [featureArea]);
+
     return (
         <>
-            <div>Feature Area: {featureArea} Square Feet.</div>
+            <div>Feature Area: {displayArea} square foot.</div>
             {MemoizedMap}
         </>
     );
